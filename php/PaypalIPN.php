924d4ea8521d89ac0c2de470b1ab87efa137a740
Suman2525 <63346552+Suman2525@users.noreply.github.com>
2020-04-09 18:52:34 +0530

    Update PaypalIPN.php

--- php/PaypalIPN.php
+++ php/PaypalIPN.php
@@ -8,7 +8,7 @@ class PaypalIPN
     private $use_local_certs = true;
 
     /** Production Postback URL */
-    const VERIFY_URI = 'https://ipnpb.paypal.com/cgi-bin/webscr';
+//     const VERIFY_URI = 'https://ipnpb.paypal.com/cgi-bin/webscr';
     /** Sandbox Postback URL */
     const SANDBOX_VERIFY_URI = 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr';
 
